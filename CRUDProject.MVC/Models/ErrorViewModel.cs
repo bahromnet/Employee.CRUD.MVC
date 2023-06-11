1e4974d0b9af252a34580c9a33acd392894fda12
Akramov_Dev <Akramov_Dev@HOME-PC>
2023-06-11 08:22:33 +0300

    Add project files.

---
+++ CRUDProject.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDProject.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
