b85b8f28eef50944ec84b4d907857208568e72d2
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-06-12 20:55:46 +0300

    some things have changed

--- Application.MVC/Commons/Models/EmployeeDto.cs
+++ Application.MVC/Commons/Models/EmployeeDto.cs
@@ -6,7 +6,7 @@ public class EmployeeDto
 {
     public Guid Id { get; set; }
     public string FullName { get; set; }
-    public Guid DepartmentId { get; set; }
+    public Department Department { get; set; }
     public decimal Salary { get; set; }
     public DateTime HireDate { get; set; }
 }
--- CRUDProject.MVC/Controllers/EmployeeController.cs
+++ CRUDProject.MVC/Controllers/EmployeeController.cs
@@ -10,23 +10,21 @@ namespace CRUDProject.MVC.Controllers
         public async Task<IActionResult> Index(int pg = 1)
         {
             const int pageSize = 10;
-
             if (pg < 1)
                 pg = 1;
-
             int recsCount = context.Employees.Count();
-
             var paginatedList = new PaginatedList(recsCount, pg, pageSize);
-
             int recSkip = (pg - 1) * pageSize;
-
             IEnumerable<EmployeeDto> allEmployees = await mediator.Send(new GetAllEmployeeQuery());
-
             List<EmployeeDto> employees = allEmployees.Skip(recSkip).Take(paginatedList.PageSize).ToList();
-
-            this.ViewBag.PaginatedList = paginatedList;
-
+            ViewBag.PaginatedList = paginatedList;
             return View(employees);
+        }
+
+        public async Task<IActionResult> Create(EmployeeDto employee)
+        {
+            await mediator.Send(employee);
+            return RedirectToAction("index");
         }
     }
 }
